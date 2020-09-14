f95af6e923c5c08c33b7e2a4b886be143e0e9148
Gaurav Naresh Pandit <57807284+gnpandit12@users.noreply.github.com>
2020-09-14 15:37:30 +0530

    Fifth_commit

--- app/src/main/java/com/example/allfriendsstudio/view/FriendsListActivity.java
+++ app/src/main/java/com/example/allfriendsstudio/view/FriendsListActivity.java
@@ -7,6 +7,8 @@ import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -15,8 +17,10 @@ import android.widget.ProgressBar;
 import com.example.allfriendsstudio.R;
 import com.example.allfriendsstudio.databinding.ActivityFriendsListBinding;
 import com.example.allfriendsstudio.model.adapter.FriendsListAdapter;
+import com.example.allfriendsstudio.model.pojoClass.Friend;
 import com.example.allfriendsstudio.viewModel.FriendsListViewModel;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.BlockingDeque;
 
@@ -29,6 +33,7 @@ public class FriendsListActivity extends AppCompatActivity {
     private FriendsListAdapter friendsListAdapter;
     private ProgressBar loadingProgressBar;
     private EditText searchUserNameEditText;
+    private List<Friend> friendList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +48,43 @@ public class FriendsListActivity extends AppCompatActivity {
         friendsListViewModel.getFriendsList();
         observeViewModel();
 
+        searchUserNameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                searchUser(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+    }
+
+    private void searchUser(String userName) {
+        List<Friend> temp = new ArrayList();
+        for (Friend friend : friendList) {
+            //or use .equal(text) with you want equal match
+            //use .toLowerCase() for better matches
+            if (friend.getUserName().contains(userName)) {
+                temp.add(friend);
+            }
+        }
+        //update recyclerview
+        updateList(temp);
+    }
+
+    public void updateList(List<Friend> friends_list) {
+        friendList = friends_list;
+        friendsListAdapter = new FriendsListAdapter(this, friendList);
+        friendsListRecyclerView.setAdapter(friendsListAdapter);
+        friendsListAdapter.notifyDataSetChanged();
     }
 
     private void observeViewModel() {
@@ -67,6 +109,7 @@ public class FriendsListActivity extends AppCompatActivity {
                         responseData -> {
                             Log.d(TAG, "onChanged: ");
                             if (responseData != null) {
+                                friendList = responseData.getFriends();
                                 friendsListAdapter = new FriendsListAdapter(this, responseData.getFriends());
                                 friendsListRecyclerView.setAdapter(friendsListAdapter);
                                 friendsListAdapter.notifyDataSetChanged();
